9500c1a6940d46917b6cbd7f054deef6eb56086d
dingqing.he <dingqing.he@stud.uni-goettingen.de>
2023-03-07 02:47:42 +0100

    use cloud db

--- backend/app.js
+++ backend/app.js
@@ -1,46 +1,48 @@
 const express = require('express')
 const mysql = require('mysql2')
+const nodemailer = require("nodemailer");
+var fs = require("fs")
+
 const app = express()
 const port = 3000
-const allowedValue = ["cat", "dog"]
 
 // connect to db
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'HDQeing110!',
-  database: 'data'
+  host: 'dinner-hopping-demo.mysql.database.azure.com',
+  user: 'dbadmin',
+  password: 'HEdingqing&ZHANGzeyu',
+  database: 'data_demo',
+  port:3306,
+  ssl:{
+    rejectUnauthorized: false
+  }
 })
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.json({"message":'Hello World!'})
-})
-
-app.get('/address', (req, res) => {
-    connection.connect()
-    connection.query('SELECT * FROM address', (err, rows, fields) => {
-  if (err) throw err
-
-  res.json({"street":rows[0].street,"house number":rows[0].housenumber})
-})
-    connection.end()
+  res.json({"message":'This is API service for dinner hopping web application!'})
 })
 
 app.post('/register', (req, res)=>{
-    console.log(allowedValue.includes(req.body.animal))
-    res.json({"message":"your animal is allowed"})
-/*
-    if (req.body.animal in allowedValue){
-        res.json({"message":"your animal is allowed"})
-    } else {
-        res.json({"message":"your animal is not allowed"})
+  
+  let stmt = `INSERT INTO participant(name,email,phonenumber,vegan,vegetarian,englishSpeaker,germanSpeaker,street,houseNumber,host,appetizer,mainCourse,dessert,comment,verified,paid)
+            VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,0,0)`;
+  let participant_info = [req.body.name,req.body.email,req.body.phonenumber,req.body.vegan,req.body.vegetarian,req.body.englishSpeaker,req.body.germanSpeaker,req.body.street,req.body.houseNumber,req.body.host, req.body.appetizer, req.body.mainCourse, req.body.dessert, req.body.comment];
+
+  // execute the insert statment
+  connection.query(stmt, participant_info, (err, results, fields) => {
+    if (err) {
+      res.json({"message":"Your info cannot be registered in system, please check your input."})
+      return console.error(err.message);
     }
-*/
-   
+    // get inserted id
+    res.json({"message":"Conguratulations, you have registered successfully!"})
+    console.log('Participant added with Id:' + results.insertId);
+  });
+
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Dinner hopping API service listening on port ${port}`)
 })
