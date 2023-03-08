1a474e65620381215cd5ab1d3e4adabc0ef257b1
dingqing.he <dingqing.he@stud.uni-goettingen.de>
2023-03-08 13:21:28 +0100

    send register from frontend

--- frontend/demo/singleregisterpage.js
+++ frontend/demo/singleregisterpage.js
@@ -2,7 +2,7 @@ const btnSubmit = document.querySelector('#button-submit');
 const tfName = document.getElementById('textfield-name');
 const tfEmail = document.getElementById('textfield-email');
 const tfPhone = document.getElementById('textfield-phone');
-const radioHost = document.getElementsByName('host');
+const radioHost = document.querySelector('input[name="host"]:checked')
 const tfStreet = document.getElementById('textfield-street');
 const tfHouseNumber = document.getElementById('textfield-housenumber');
 const ckboxVegetarian = document.getElementById('vegetarian');
@@ -13,40 +13,30 @@ const ckboxAppetizer = document.getElementById('appetizer');
 const ckboxMainCourse = document.getElementById('mainCourse');
 const ckboxDessert = document.getElementById('dessert');
 const tfComment = document.getElementById('comment');
-const registerForm = document.getElementById('register-form');
 
-
-function sendData(){
-    let formData = new FormData();
-    formData.append("name", tfName.value);
-    formData.append('email', tfEmail.value);
-    formData.append('phonenumber', tfPhone.value)
-    formData.append('host', parseInt(radioHost.value));
-    formData.append('street', tfStreet.value);
-    formData.append('houseNumber', tfHouseNumber.value);
+function sendData(event){
+    event.preventDefault();
     let vegetarian = 0;
     let vegan = 0;
+    let englishSpeaker = 0;
+    let germanSpeaker = 0;
+    let appetizer = 0;
+    let mainCourse = 0;
+    let dessert = 0;
+    let host = 0;
+    let housenumber=0;
     if (ckboxVegetarian.checked){
         vegetarian = 1;
     }
     if (ckboxVegan.checked){
         vegan = 1;
     }
-    formData.append('vegetarian', vegetarian);
-    formData.append('vegan', vegan);
-    let englishSpeaker = 0;
-    let germanSpeaker = 0;
     if (ckboxEnglish.checked){
         englishSpeaker = 1;
     }
     if (ckboxGerman.checked){
         germanSpeaker = 1;
     }
-    formData.append('englishSpeaker', englishSpeaker);
-    formData.append('germanSpeaker', germanSpeaker);
-    let appetizer = 0;
-    let mainCourse = 0;
-    let dessert = 0;
     if (ckboxAppetizer.checked){
         appetizer = 1;
     }
@@ -56,38 +46,38 @@ function sendData(){
     if (ckboxDessert.checked){
         dessert = 1;
     }
-    formData.append('appetizer', appetizer);
-    formData.append('mainCourse', mainCourse);
-    formData.append('dessert', dessert);
-    formData.append('comment', tfComment.value);
-    /*
-    fetch("dinner-hopping.azurewebsites.net/register", {
+    host = parseInt(radioHost.value);
+    housenumber = parseInt(tfHouseNumber.value);
+
+    info = {
+        "name": tfName.value,
+        'email': tfEmail.value,
+        'phonenumber': tfPhone.value,
+        'host': host,
+        'street': tfStreet.value,
+        'houseNumber': housenumber,
+        'vegetarian': vegetarian,
+        'vegan': vegan,
+        'englishSpeaker': englishSpeaker,
+        'germanSpeaker': germanSpeaker,
+        'appetizer': appetizer,
+        'mainCourse': mainCourse,
+        'dessert': dessert,
+        'comment': tfComment.value
+    };
+
+    console.log(info);
+    fetch("https://dinner-hopping.azurewebsites.net/register", {
         method:"POST",
-        
         headers:{
             'Accept': 'application/json',
             'Content-Type': 'application/json'
-
         },
-        
-        body: JSON.stringify(formData)
-    }).then(function(res){ return res.json(); })
-    .then(function(data){ alert( JSON.stringify( data ) ) })
-    */
-    const myHeaders = {}
-    const myInit = {
-    method: "GET",
-    mode:'cors',
-    headers: myHeaders,
-    credentials: "include"
-    }
-    fetch("https://dinner-hopping.azurewebsites.net/allow-cors", myInit).then(function(res){ return res.json(); })
-    .then(function(data){ alert( JSON.stringify( data ) ) })
+        body: JSON.stringify(info)
+    }).then(res => res.json())
+    .then(data=> console.log(data));
 
 };
 
-registerForm.addEventListener("submit", ()=>{
-    fetch("https://dinner-hopping.azurewebsites.net/")
-    .then(res => res.json())
-    .then(data => console.log(data))
-});
+const buttonSubmit = document.getElementById('button-submit')
+buttonSubmit.addEventListener("click", sendData, false)
