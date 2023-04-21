9cbf03b91b26a36a52739033ea489007ceaae02a
geekziyu <geekziyu@163.com>
2023-04-21 21:53:57 +0800

    build & commit dist

---
+++ frontend/dist/assets/ForOneself-a91714bd.js
@@ -0,0 +1,1 @@
+import{E as e}from"./Enrollment-b1733c06.js";import{f as o,o as r,g as n}from"./index-5054acd1.js";const p=o({__name:"ForOneself",setup(t){return(a,s)=>(r(),n(e))}});export{p as default};
